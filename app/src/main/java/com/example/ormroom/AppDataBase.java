8c1a45eac8d6fab0d2765ace49a6dd6e35fbad13
anggonzales <anggonzalesca@upt.pe>
2020-02-19 11:17:15 -0500

    ORM

---
+++ app/src/main/java/com/example/ormroom/AppDataBase.java
@@ -0,0 +1,12 @@
+package com.example.ormroom;
+
+
+import androidx.room.Database;
+import androidx.room.RoomDatabase;
+import androidx.room.TypeConverters;
+
+@Database(entities = {Contacto.class}, version = 1, exportSchema = false)
+@TypeConverters({DateTypeConverter.class})
+public abstract class AppDataBase extends RoomDatabase {
+    public abstract ContactDAO getContactDAO();
+}
